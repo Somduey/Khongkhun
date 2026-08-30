fb65027f857ccde09a17e36c091c47c83817e8e7
SomsakSree <Thabphan1311@gmail.com>
2026-08-30 23:05:12 +0700

    update 3.0

--- src/services/firebase.js
+++ src/services/firebase.js
@@ -10,11 +10,9 @@ import {
   signOut,
 } from "firebase/auth";
 
-const firebaseAuthDomain = "khongkhun-e6def.firebaseapp.com";
-
 const firebaseConfig = {
   apiKey: "AIzaSyBmHf7mKH846i87MsSZ7LngQ7pT5so7mmY",
-  authDomain: import.meta.env.DEV ? firebaseAuthDomain : window.location.host,
+  authDomain: "khongkhun-e6def.firebaseapp.com",
   projectId: "khongkhun-e6def",
   storageBucket: "khongkhun-e6def.firebasestorage.app",
   messagingSenderId: "260566796572",
